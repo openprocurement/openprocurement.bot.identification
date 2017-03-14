b5116478993e4d68d5941fe81fc6e7be270707c5
Krokop <chezar1995@gmail.com>
2017-03-14 17:11:51 +0200

    edit doc strings and add test for check forward work

--- openprocurement/bot/identification/tests/scanner.py
+++ openprocurement/bot/identification/tests/scanner.py
@@ -140,7 +140,7 @@ class TestScannerWorker(unittest.TestCase):
 
     @patch('gevent.sleep')
     def test_backward_dead(self, gevent_sleep):
-        """Three times receive 425, check queue, check sleep_change_value"""
+        """ Restart worker after getting 403 """
         gevent_sleep.side_effect = custom_sleep
         tender_queue = Queue(10)
         client = MagicMock()
@@ -173,4 +173,38 @@ class TestScannerWorker(unittest.TestCase):
         del worker
         self.assertEqual(tender_queue.qsize(), 3)
 
+    @patch('gevent.sleep')
+    def test_forward_run(self, gevent_sleep):
+        """  Run forward when backward get empty response and
+            prev_page.offset is equal to next_page.offset """
+        gevent_sleep.side_effect = custom_sleep
+        tender_queue = Queue(10)
+        client = MagicMock()
+        client.sync_tenders.side_effect = [
+            munchify({'prev_page': {'offset': '123'},
+                      'next_page': {'offset': '1234'},
+                      'data': [{'status': "active.pre-qualification",
+                                "id": uuid.uuid4().hex,
+                                'procurementMethodType': 'aboveThresholdEU'}]}),
+            munchify({'prev_page': {'offset': '1234'},
+                      'next_page': {'offset': '1235'},
+                      'data': [{'status': "active.pre-qualification",
+                                "id": uuid.uuid4().hex,
+                                'procurementMethodType': 'aboveThresholdEU'}]}),
+            munchify({'prev_page': {'offset': '1235'},
+                      'next_page': {'offset': '1235'},
+                      'data': []}),
+            munchify({'prev_page': {'offset': '1236'},
+                      'next_page': {'offset': '1237'},
+                      'data': [{'status': "active.pre-qualification",
+                                "id": uuid.uuid4().hex,
+                                'procurementMethodType': 'aboveThresholdEU'}]})]
+
+        worker = Scanner.spawn(client, tender_queue, 1, 0.5)
+        sleep(5)
+
+        # Kill worker
+        worker.shutdown()
+        del worker
+        self.assertEqual(tender_queue.qsize(), 5)
 
