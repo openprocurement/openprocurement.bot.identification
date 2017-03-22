e7e1a52babe1687e34bfbc64256a4c3317b70a60
Iryna <iryna.pomazan@unity-bars.com>
2017-03-22 17:27:42 +0200

    Change Authentication in DocServiceClient. Fix bugs

--- openprocurement/bot/identification/tests/upload_file.py
+++ openprocurement/bot/identification/tests/upload_file.py
@@ -37,7 +37,7 @@ class TestUploadFileWorker(unittest.TestCase):
     @patch('gevent.sleep')
     def test_successful_upload(self, mrequest, gevent_sleep):
         gevent_sleep.side_effect = custom_sleep
-        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', token='')
+        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', user='', password='')
         mrequest.post('{url}'.format(url=doc_service_client.url),
                       json={'data': {'url': 'http://docs-sandbox.openprocurement.org/get/8ccbfde0c6804143b119d9168452cb6f',
                                     'format': 'text/plain',
@@ -72,7 +72,7 @@ class TestUploadFileWorker(unittest.TestCase):
     @patch('gevent.sleep')
     def test_retry_doc_service(self, mrequest, gevent_sleep):
         gevent_sleep.side_effect = custom_sleep
-        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', token='')
+        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', user='', password='')
         mrequest.post('{url}'.format(url=doc_service_client.url),
                       [{'text': '', 'status_code': 401},
                        {'text': '', 'status_code': 401},
@@ -108,12 +108,12 @@ class TestUploadFileWorker(unittest.TestCase):
     @patch('gevent.sleep')
     def test_retry_upload_to_tender(self, mrequest, gevent_sleep):
         gevent_sleep.side_effect = custom_sleep
-        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', token='')
+        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', user='', password='')
         mrequest.post('{url}'.format(url=doc_service_client.url),
                       json={'data': {'url': 'http://docs-sandbox.openprocurement.org/get/8ccbfde0c6804143b119d9168452cb6f',
-                                    'format': 'text/plain',
+                                    'format': 'application/json',
                                     'hash': 'md5:9a0364b9e99bb480dd25e1f0284c8555',
-                                    'title': 'file.txt'}},
+                                    'title': 'edr_request.json'}},
                       status_code=200)
         client = MagicMock()
         client._create_tender_resource_item.side_effect = [Unauthorized(),
