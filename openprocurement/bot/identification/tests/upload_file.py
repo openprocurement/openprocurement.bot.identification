981bb6b54bd0cb37c8d9caae00700713f71af5fc
Iryna <iryna.pomazan@unity-bars.com>
2017-04-05 16:01:08 +0300

    Skip tender, when accept 422 error code while uploading document

--- openprocurement/bot/identification/tests/upload_file.py
+++ openprocurement/bot/identification/tests/upload_file.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from gevent import monkey
+monkey.patch_all()
+
 import uuid
 import unittest
 import datetime
@@ -7,6 +10,7 @@ from gevent.queue import Queue
 from time import sleep
 from mock import patch, MagicMock
 from restkit.errors import Unauthorized
+from restkit import ResourceError
 
 from openprocurement.bot.identification.client import DocServiceClient
 from openprocurement.bot.identification.databridge.upload_file import UploadFile
@@ -120,12 +124,12 @@ class TestUploadFileWorker(unittest.TestCase):
                                     'title': 'edr_request.yaml'}},
                       status_code=200)
         client = MagicMock()
-        client._create_tender_resource_item.side_effect = [Unauthorized(),
-                                                           Unauthorized(),
-                                                           Unauthorized(),
-                                                           Unauthorized(),
-                                                           Unauthorized(),
-                                                           Unauthorized(),
+        client._create_tender_resource_item.side_effect = [Unauthorized(http_code=403),
+                                                           Unauthorized(http_code=403),
+                                                           Unauthorized(http_code=403),
+                                                           Unauthorized(http_code=403),
+                                                           Unauthorized(http_code=403),
+                                                           Exception(),
                                                            {'data': {'id': uuid.uuid4().hex,
                                                                      'documentOf': 'tender',
                                                                      'documentType': 'registerExtract',
@@ -148,3 +152,65 @@ class TestUploadFileWorker(unittest.TestCase):
         self.assertEqual(processing_items, {})  # test that item removed from processing_items
         self.assertEqual(client._create_tender_resource_item.call_count, 7)  # check upload to tender
 
+    @requests_mock.Mocker()
+    @patch('gevent.sleep')
+    def test_request_failed(self, mrequest, gevent_sleep):
+        gevent_sleep.side_effect = custom_sleep
+        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', user='', password='')
+        mrequest.post('{url}'.format(url=doc_service_client.url),
+                      json={'data': {'url': 'http://docs-sandbox.openprocurement.org/get/8ccbfde0c6804143b119d9168452cb6f',
+                                    'format': 'application/yaml',
+                                    'hash': 'md5:9a0364b9e99bb480dd25e1f0284c8555',
+                                    'title': 'edr_request.yaml'}},
+                      status_code=200)
+        client = MagicMock()
+        client._create_tender_resource_item.side_effect = ResourceError(http_code=422)
+        tender_id = uuid.uuid4().hex
+        award_id = uuid.uuid4().hex
+        processing_items = {award_id: tender_id}
+        upload_to_doc_service_queue = Queue(10)
+        upload_to_tender_queue = Queue(10)
+        upload_to_doc_service_queue.put(Data(tender_id, award_id, '123', 'awards', None, {'test_data': 'test_data'}))
+        worker = UploadFile.spawn(client, upload_to_doc_service_queue, upload_to_tender_queue, processing_items, doc_service_client)
+        sleep(10)
+        worker.shutdown()
+        self.assertEqual(upload_to_doc_service_queue.qsize(), 0, 'Queue should be empty')
+        self.assertEqual(upload_to_tender_queue.qsize(), 0, 'Queue should be empty')
+        self.assertEqual(mrequest.call_count, 1)
+        self.assertEqual(mrequest.request_history[0].url, u'127.0.0.1:80/upload')
+        self.assertEqual(processing_items, {})
+        self.assertEqual(client._create_tender_resource_item.call_count, 1)  # check that processed just 1 request
+
+    @requests_mock.Mocker()
+    @patch('gevent.sleep')
+    def test_request_failed_in_retry(self, mrequest, gevent_sleep):
+        gevent_sleep.side_effect = custom_sleep
+        doc_service_client = DocServiceClient(host='127.0.0.1', port='80', user='', password='')
+        mrequest.post('{url}'.format(url=doc_service_client.url),
+                      json={'data': {'url': 'http://docs-sandbox.openprocurement.org/get/8ccbfde0c6804143b119d9168452cb6f',
+                                    'format': 'application/yaml',
+                                    'hash': 'md5:9a0364b9e99bb480dd25e1f0284c8555',
+                                    'title': 'edr_request.yaml'}},
+                      status_code=200)
+        client = MagicMock()
+        client._create_tender_resource_item.side_effect = [ResourceError(http_code=425),
+                                                           ResourceError(http_code=422),
+                                                           ResourceError(http_code=422),
+                                                           ResourceError(http_code=422),
+                                                           ResourceError(http_code=422),
+                                                           ResourceError(http_code=422)]
+        tender_id = uuid.uuid4().hex
+        award_id = uuid.uuid4().hex
+        processing_items = {award_id: tender_id}
+        upload_to_doc_service_queue = Queue(10)
+        upload_to_tender_queue = Queue(10)
+        upload_to_doc_service_queue.put(Data(tender_id, award_id, '123', 'awards', None, {'test_data': 'test_data'}))
+        worker = UploadFile.spawn(client, upload_to_doc_service_queue, upload_to_tender_queue, processing_items, doc_service_client)
+        sleep(10)
+        worker.shutdown()
+        self.assertEqual(upload_to_doc_service_queue.qsize(), 0, 'Queue should be empty')
+        self.assertEqual(upload_to_tender_queue.qsize(), 0, 'Queue should be empty')
+        self.assertEqual(mrequest.call_count, 1)
+        self.assertEqual(mrequest.request_history[0].url, u'127.0.0.1:80/upload')
+        self.assertEqual(processing_items, {})
+        self.assertEqual(client._create_tender_resource_item.call_count, 6)  # check that processed just 1 request
