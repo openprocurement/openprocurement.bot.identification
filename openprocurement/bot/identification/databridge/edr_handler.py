299b2d5aef5be7824ad2df7b4fb32decd0974238
Iryna <iryna.pomazan@unity-bars.com>
2017-05-05 11:15:47 +0300

    fix no global name re

--- openprocurement/bot/identification/databridge/edr_handler.py
+++ openprocurement/bot/identification/databridge/edr_handler.py
@@ -222,7 +222,7 @@ class EdrHandler(Greenlet):
             for edr_id in tender_data.edr_ids:
                 try:
                     response = self.get_edr_details_request(edr_id)
-                except RetryException:
+                except RetryException as re:
                     self.retry_edr_ids_queue.put((Data(tender_data.tender_id, tender_data.item_id, tender_data.code,
                                                        tender_data.item_name, [edr_id], tender_data.file_content)))
                     logger.info('Put tender {} with {} id {} to retry_edr_ids_queue.Error response {}'.format(
